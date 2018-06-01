457454a5a931198779ceef780c295fe60d07f7ca
Chris Jacob <i.chris.jacob@gmail.com>
2018-06-01 23:58:23 +1000

    Revert "test commit via api"
    This reverts commit a7de9f726fe6682b6fdc36b70708ed734a3cc2c2.

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+console.log('Links...');
+
+var APP = {};
+
+// https://davidwalsh.name/query-string-javascript
+function getUrlParameter(name) {
+    name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+    var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+    var results = regex.exec(location.search);
+    return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+APP.url = getUrlParameter('url'); // "?url=https://google.com" -> "https://google.com"
+
+console.log('URL:');
+console.log(APP.url);
